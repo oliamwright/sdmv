5da0f94d38d2437792f17c95c0895bc157056756
Alex Etkin <alexetkin@users.noreply.github.com>
2015-05-30 09:46:24 +0500

    Edit models PersonValue, Venue

--- app/models/person_value.rb
+++ app/models/person_value.rb
@@ -1,6 +1,12 @@
+require 'time_range'
+
 class PersonValue < ActiveRecord::Base
   validates :x, presence: true
   validates :y, presence: true
 
   has_many :venue_person_values, dependent: :destroy
+
+  def time_range
+    TimeRange.new(from, to)
+  end
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,12 @@
+require 'time_range'
+
 class Venue < ActiveRecord::Base
   validates :x, presence: true
   validates :y, presence: true
 
   has_many :venue_person_values, dependent: :destroy
+
+  def time_range
+    TimeRange.new(from, to)
+  end
 end
