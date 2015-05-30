6fdfbc593fd721133e7b585d9477d8e03f524ca9
Alex Etkin <alexetkin@users.noreply.github.com>
2015-05-30 12:09:32 +0500

    Add method PersonValue#available_for_venue?

--- app/models/person_value.rb
+++ app/models/person_value.rb
@@ -2,6 +2,7 @@ require 'time_range'
 
 class PersonValue < ActiveRecord::Base
   VELOCITY = 50.0 / 1.hour # 50 kilometers per hour
+  MINIMUM_TIME_AT_VENUE = 30.minutes
 
   validates :x, presence: true
   validates :y, presence: true
@@ -14,5 +15,17 @@ class PersonValue < ActiveRecord::Base
 
   def time_to_venue(venue)
     Math.sqrt((x - venue.x)**2 + (y - venue.y)**2) / VELOCITY
+  end
+
+  def time_range_at_venue(venue)
+    time_to_arrive = time_to_venue(venue)
+    TimeRange.new(from + time_to_arrive, to - time_to_arrive)
+  rescue ArgumentError
+    TimeRange.new
+  end
+
+  def available_for_venue?(venue)
+    MINIMUM_TIME_AT_VENUE <=
+      time_range_at_venue(venue).intersection(venue.time_range).seconds
   end
 end
