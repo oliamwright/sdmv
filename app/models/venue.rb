4c1c4a775acd653631a3c6ca6665b523dcafd260
Alex Etkin <alexetkin@users.noreply.github.com>
2015-06-07 12:27:08 +0500

    Add relation between Venue and TimeRange

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -6,6 +6,8 @@ class Venue < ActiveRecord::Base
 
   has_many :venue_person_values, dependent: :destroy
 
+  has_many :time_ranges, as: :owner
+
   def time_range
     TimeRange.new(from, to)
   end
